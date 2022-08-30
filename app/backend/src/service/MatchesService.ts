d3ebc561701044514a3ad0f1da4c2a95ca97f350
IgorHamzi <igorwhamzi@gmail.com>
2022-08-30 15:56:53 -0300

    feat: validates match creation with team that does not exist in the database

--- app/backend/src/service/MatchesService.ts
+++ app/backend/src/service/MatchesService.ts
@@ -25,12 +25,23 @@ class MatchesService {
   };
 
   public createMatch = async (match: IMatches) => {
+    const awayTeam = await TeamModel.findOne({ where: { id: match.awayTeam } });
+    const homeTeam = await TeamModel.findOne({ where: { id: match.homeTeam } });
+
+    if (!awayTeam || homeTeam) {
+      throw new CustomError(
+        404,
+        'There is no team with such id!',
+      );
+    }
+
     if (match.homeTeam === match.awayTeam) {
       throw new CustomError(
         401,
         'It is not possible to create a match with two equal teams',
       );
     }
+
     const newMatche = await MatchesModel.create({ ...match, inProgress: true });
     return newMatche;
   };
